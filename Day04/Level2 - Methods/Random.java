5dac9e33d8ad269066cfd7c42db1623b47bfdeda
Anshik01 <anshik0706@gmail.com>
2025-01-12 22:52:56 +0530

    Random.java file in Day04-level2 updated file added

--- Day04/Level2 - Methods/Random.java
+++ Day04/Level2 - Methods/Random.java
@@ -1,20 +1,28 @@
 import java.lang.Math;
 
-public class GreatestFactor {
+public class Random {
 	
 	static float[] randomValues() {
 		// created four integer variables, and initialized them with random values
-		int first = Math.random() % 10;
-		int second = Math.random() % 10;
-		int third = Math.random() % 10;
-		int fourth = Math.random() % 10;
+		int first = (int)(Math.random() * 9) + 1;
+		int second = (int)(Math.random() * 9) + 1;
+		int third = (int)(Math.random() * 9) + 1;
+		int fourth = (int)(Math.random() * 9) + 1;
 		
-		float average = (float)(first + second + third + fourth) / 4f;
+		float averageRandom = (float)(first + second + third + fourth) / 4f;
+		float minimumRandom = Math.min(Math.min(first, second), Math.min(third, fourth));
+		float maximumRandom = Math.max(Math.max(first, second), Math.max(third, fourth));
 		
+		float[] randomValues = {averageRandom, minimumRandom, maximumRandom};
+		return randomValues;
 	}		
 	
 	public static void main(String[] args){
-		float[] randomValues = getRandom();
+		float[] randomValues = randomValues();
 		
+		// printing the output
+		System.out.println("Random values average: " + randomValues[0]);
+		System.out.println("Random values minimum: " + (int)randomValues[1]);
+		System.out.println("Random values maximum: " + (int)randomValues[2]);
 	}
 }
